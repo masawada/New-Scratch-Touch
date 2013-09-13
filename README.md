0868933410bea4eb3d84968d8164504d593aabbc
Masayoshi Wada <developer@andantesoftware.com>
2013-09-14 08:33:09 +0900

    Add some files and Fix a bug
    + add README
    + add client side files
      + view
      + controller
    + express.createServer is deprecated

---
+++ README.md
@@ -0,0 +1,40 @@
+# Scratch Touch
+
+Scratch (1.4)では外部センサーを使うことが出来ます。これは、iPhoneをセンサー代わりにするデモです。
+
+すでに2年前[Scratch Touch](https://github.com/masawada/scratch-touch)を開発していますが、気付いたらexpressとsocket.ioが気付いたら劇的な仕様変更をしていて変更するのが面倒になったので一から書きました。
+
+## 必要なもの
+* iPhone (iPod touch)
+* Node
+* npm
+* Scratch (1.4)
+
+iPhoneをLAN経由でPCにつなぎ、NodeがScratchとiPhoneの間を仲介します。
+Nodeとnpmを前もってインストールしてください。
+（MacでNodeを扱うならHomebrewがオススメです。）
+
+## 使い方
+1. Nodeとnpmをインストールしましょう
+2. ターミナル（端末・コマンドプロンプト）から以下のコマンドを入力します
+`git clone https://github.com/masawada/New-Scratch-Touch.git scratch-touch`
+`cd scratch-touch`
+`npm install express jade socket.io`
+`node app.js`
+3. Scratchを起動します
+4. 遠隔センサー接続をオンにします
+5. iPhoneのSafariから"http://PCの(LAN)IPアドレス:3000/"にアクセスしてください
+6. この画面を「ホーム画面に追加」して再度起動するとフルスクリーンモードで遊べます
+
+### 遠隔センサー接続をオンにする方法
+1. Scratchを起動します
+2. "調べる"ボタンをクリックします
+3. "スライダーセンサーの値"を右クリックします
+4. "遠隔センサー接続を有効にする"をクリックします
+
+### 仕組み
+
+iPhoneから受け取った値を文字列にしてUDPでローカルの42001番ポートに投げつけています。`sensor-update "ax" 0 "ay" 0 "rx" 0 "ry" 0`のような感じです。また、タッチイベントを検出した場合は`broadcast "message"`のようにメッセージを送信するように実装しています。
+
+## ライセンス
+MIT License
--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+// require modules
+var express = require('express')
+  , sio = require('socket.io')
+  , dgram = require('dgram');
+
+// create UDP socket
+var scratch_con = dgram.createSocket('udp4');
+
+// create server
+var app = module.exports = express();
+
+// prepare app and socket.io
+var io = sio.listen(app.listen(3000));
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/view');
+  app.set('view engine', 'jade');
+});
+
+app.get('/', function (req, res) {
+  res.render('index', { layout: false });
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('point-update', function (p) {
+    console.log(p);
+    var message = 'sensor-update'
+                + ' "ax" ' + p.ax
+                + ' "ay" ' + p.ay
+                + ' "rx" ' + p.rx
+                + ' "ry" ' + p.ry;
+
+    var packet = new Buffer(message);
+    scratch_con.send(packet, 0, packet.length, 42001, '127.0.0.1', function(){});
+  });
+
+  socket.on('broadcast', function(ctx){
+    var message = 'broadcast "' + ctx + '"';
+    var packet = new Buffer(message);
+    scratch_con.send(packet, 0, packet.length, 42001, '127.0.0.1', function(){});
+  });
+});
 
